dffc13391e1a2eaa92c816ff9dbcf4b30c16288a
Ben <x>
2018-09-26 10:04:44 +0200

    Creation : Model Player qui herite de User

---
+++ Archery/Models/Administrator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Archery.Models
+{
+    //public sealed class Administrator : SEALED > Fin de l'heritage, celle l'heritage du parent | confirmer la definition
+
+    public class Administrator : User
+    {
+        public
+    }
+}
