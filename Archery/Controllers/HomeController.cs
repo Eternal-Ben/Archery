df2bb6fc2e162dd9f6f4524f1c78845eac60a07c
Eternal-Ben <Eternalst@live.fr>
2018-09-26 07:30:08 +0200

    Creation : HomeController, ajout d'un ActionResult Challenge, Todo : creer la View Challenge

--- Archery/Controllers/HomeController.cs
+++ Archery/Controllers/HomeController.cs
@@ -21,10 +21,10 @@ namespace Archery.Controllers
             return View();
         }
 
-        // On indique le chemin de la route par une propriété ci dessous
+        // On indique le chemin de la route par une propriété ci dessous, autre technique
         //[Route("a-propros")]
         // Vue about : pour la creation de la View... About donc...
-        public  ActionResult About()
+        public ActionResult About()
         {
             // permet le titrage de la page web dynamiquement
             ViewData["Title"] = "About";
@@ -41,6 +41,18 @@ namespace Archery.Controllers
           /// si remplacement d'attributs avec les models geré par les controllers, preciser le parametre genre
           /// nom de l'objet [parametre]
           ///
+        }
+
+
+        //Un actionresult Challenge pour la creation du contenu de la View, il faut penser d'abord à creer son model avec ses paramêtres dans les Views
+        public ActionResult Challenge()
+        {
+            ViewData["Challenge"] = "Challenge";
+            var modelChallenge = new Challenge
+            //{
+            //    parametre des variables à ajouter, puis creer un View Challenge pour l'affichage 
+            //};
+            return View();
         }
     }
 }
