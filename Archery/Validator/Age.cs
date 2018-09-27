878ec5442cf91e81fcd84ba8f759ed52f8c27572
Ben <x>
2018-09-27 12:15:26 +0200

    Corection Methode : MinimumAge, MaximumAge, IsValide

--- Archery/Validator/Age.cs
+++ Archery/Validator/Age.cs
@@ -6,9 +6,18 @@ using System.Web;
 
 namespace Archery.Views.Validator
 {
+    [AttributeUsage(AttributeTargets.Property)] // Sinon declarer la class en Sealed
     public class Age : ValidationAttribute
     {
-        public  int MinimumAge { get; private set; }
+        public int MinimumAge { get; private set; }
+        public int? maximumAge;
+
+
+        public int MaximumAge
+        {
+            get { return (int)maximumAge; } // casting de données ?
+            set { maximumAge = value; }
+        }
 
         public Age(int minimumAge) // Constructeur de la class Age qui herite de la class ValidationAttribute
         {
@@ -17,17 +26,27 @@ namespace Archery.Views.Validator
 
         public override bool IsValid(object value)
         {
-            if (value is DateTime)
+            if (value is DateTime);
+
             {
-                return DateTime.Now.AddYears(-MinimumAge) <= (DateTime)value;
-            }
-            else
+                if (value is DateTime)
+                {
+                    if (this.maximumAge == null)
+                        return DateTime.Now.AddYears(-this.MinimumAge) >= (DateTime)value;
+                    else
+                        return DateTime.Now.AddYears(-this.MinimumAge) >= (DateTime)value
+                            && ((DateTime)value).AddYears(this.MaximumAge) >= DateTime.Now;
+                }
+                else
                 throw new ArgumentException("Le type doit être un DateTime"); // retour message error
+            }
         }
 
         public override string FormatErrorMessage(string name)
         {
-            return string.Format(this.ErrorMessage, name, this.MinimumAge.ToString());
+            if (this.maximumAge == null)
+                return string.Format(this.ErrorMessage, name, this.MinimumAge.ToString());
+            return string.Format(this.ErrorMessage, name, this.MinimumAge.ToString(), this.MaximumAge.ToString());
         }
     }
 }
