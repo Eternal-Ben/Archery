063b2b93bdb8ace2b3ca201b923d5372b60e7d87
anneprt <anne.parrot82@gmail.com>
2018-10-04 17:04:48 +0200

    Ajouts d'un Include

--- Archery/Controllers/HomeController.cs
+++ Archery/Controllers/HomeController.cs
@@ -6,48 +6,32 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace Archery.Controllers
-{   // "nom de notre classe qui ": > herite" d'une class mère Controller, il ne s'agit pas d'une interface, sinon il y aurait un HomeController : IController...
+{
     public class HomeController : BaseController
     {
         // GET: Home
-        // Appel le resulat de la methode Index qui est de type ActionResult qui est en acces public
         public ActionResult Index()
         {
-            // ViewBag (dynamique) : Recupère le dictionnaire de données d'affichage de facon dynamique.et va garantir l'echange des donnees durant le cicle d'execution de la View (<?).
-            // ViewData (non-dynamique)  : Recupère ou définit le dictionnaire pour les données d'affichage durant le cycle d'execution de la View.
-
-            ViewData["Title"] = "Accueil"; // peut charger des objets, mais attention uniquement des objets simple.
-
+            ViewData["Title"] = "Accueil";
             HomeIndexViewModel model = new HomeIndexViewModel();
-            model.Tournaments = db.Tournaments.Where(x => x.StartDate >= DateTime.Now)
+            model.Tournaments = db.Tournaments.Include("Weapons")
+                                              .Where(x => x.StartDate >= DateTime.Now)
                                               .OrderBy(x => x.StartDate)
                                               .Take(20);
-
-            // retourne la parametre View de la méthode Index qui a été instancié par la methode ActionResult (confirmer la syntax)
-            // Viewresult est la class enfant de la methode ActionResult ; notion d'heritage
-            return View(20);
+            return View(model);
         }
 
-        // On indique le chemin de la route par une propriété ci dessous
-        //[Route("a-propros")]
-        // Vue about : pour la creation de la View... About donc...
-        public  ActionResult About()
+        //[Route("a-propos")]
+        public ActionResult About()
         {
-            // permet le titrage de la page web dynamiquement | dispo dans le control et les Views qui seront apl | dispo le temps d'execution du cycle partout. identique a une autre variable qui elle n'est pas dynamique mais dans le dictionnaire | passer des donnees au control ou aux vues le temps d'execution propre au controleur
-            ViewData["Title"] = "About";
-            var modelInfo = new Info //creation de l'objet Info
-
+            var modelInfo = new Info
             {
-                DevName = "Thierry",
-                ContactMail = "123@gmail.com",
-                CreateDate = DateTime.UtcNow,
-           };
-            // Pour retourner la valeur de la variable modelInfo instancié par l'objet View
+                DevName = "Vincent",
+                ContactMail = "vincent@vincent.com",
+                CreateDate = DateTime.Now
+            };
             return View(modelInfo);
-
-          /// si remplacement d'attributs avec les models geré par les controllers, preciser le parametre genre
-          /// nom de l'objet [parametre]
-          ///
         }
+
     }
 }
