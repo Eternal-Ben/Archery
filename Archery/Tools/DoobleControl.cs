383690e99418d2e91454d9fe1d8e171635a6debc
Eternal-Ben <Eternalst@live.fr>
2018-10-08 00:09:15 +0200

    Crearion : Class DoobleControl Motivation : Generalisation de class DoobleControl des nom double dans la bd A faire : Je n'y arrive pas pour le moment...

---
+++ Archery/Tools/DoobleControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Archery.Tools
+//{
+//    public class DoobleControl : ModelStateDictionary
+//    {
+//        public override bool Contains(string value)
+//    {
+//        if (value != null && value is string)
+//        {
+//            using (WeaponDbContext db = new WeaponDbContext()) ;
+//        }
+//        return db.Weapons.Any(NewOne => NewOne.Name.Equals(weapon.Name));
+//    Je n'y arrive pas pour le moment...}
+//}
+//}
